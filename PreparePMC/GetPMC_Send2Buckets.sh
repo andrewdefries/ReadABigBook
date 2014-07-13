59cdb45aabc91a73454c128b39256b9370be03db
Andrew Defries PhD <andrew.defries@gmail.com>
2014-07-12 21:40:54 -0700

    wget list ready for getting

--- PreparePMC/GetPMC_Send2Buckets.sh
+++ PreparePMC/GetPMC_Send2Buckets.sh
@@ -1,6 +1,7 @@
 #wget ftp://ftp.ncbi.nlm.nih.gov/pub/pmc/file_list.pdf.txt
 
-cat file_list.pdf.txt | sed 's/^/ftp:\/\/ftp.ncbi.nlm.nih.gov\/pub\/pmc\//' > PMCgetList
+
+cat file_list.pdf.txt | sed '1d' | sed 's/^/ftp:\/\/ftp.ncbi.nlm.nih.gov\/pub\/pmc\//' | cut -d '.' -f1-6 | sed 's/$/.pdf/' > PMCgetList
 
 #head PMCgetList
 
