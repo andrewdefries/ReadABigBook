07341dc4f998004af81fd236eac9622350090b2d
Debian <admin@ip-10-255-8-102.us-west-2.compute.internal>
2014-10-09 14:46:07 +0000

    some new error

---
+++ xmlPlay/GetData.sh
@@ -0,0 +1,6 @@
+wget ftp://ftp.ncbi.nlm.nih.gov/pub/pmc/articles.A-B.tar.gz
+wget ftp://ftp.ncbi.nlm.nih.gov/pub/pmc/articles.C-H.tar.gz
+wget ftp://ftp.ncbi.nlm.nih.gov/pub/pmc/articles.I-N.tar.gz
+wget ftp://ftp.ncbi.nlm.nih.gov/pub/pmc/articles.O-Z.tar.gz
+
+#tar -zxvf 
---
+++ xmlPlay/RunWEase.sh
@@ -0,0 +1,5 @@
+echo "Get dataset"
+
+R CMD BATCH MakeNewDb.R  
+
+R CMD BATCH ReadXML_makedb.R
